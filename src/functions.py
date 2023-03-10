a5ebe9ae7357e1e1a478fbb0d78803d47e2c2b95
Erfan Moeini <moeini.erfan@gmail.com>
2023-03-11 00:37:08 +0330

    add function calculator

--- src/functions.py
+++ src/functions.py
@@ -12,3 +12,18 @@ def division2(a, b):
 
 def remaining(a, b):
 	return a % b
+
+def calculator(a, b, action):
+	if action is 'sum2':
+		return sum2(a, b)
+	elif action is 'minus2':
+		return minus2(a, b)
+	elif action is 'multiply2':
+		return multiply2(a, b)
+	elif action is 'division2':
+		return division2(a, b)
+	elif action is 'remaining':
+		return remaining(a, b)
+	else:
+		raise 'Error: bad request'
+	
