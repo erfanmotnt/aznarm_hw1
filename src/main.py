4b8f758220b90f79669c47997dfd313777325fba
Erfan Moeini <moeini.erfan@gmail.com>
2023-03-11 00:54:53 +0330

    get input in main

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+from functions import sum2
+
 if __name__ == '__main__':
-	print('Hello')
+	inp = input("enter action:")
+	a, b = inp.strip().split(' ')
+	a, b = int(a), int(b)
+	s = sum2(a, b)
+	print(s)
