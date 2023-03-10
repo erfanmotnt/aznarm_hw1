0cc21a00ba43abf0f5c7b307a45f7440d0c005f8
Ali <ali@Alis-MacBook-Air.local>
2023-03-11 00:14:47 +0330

    refactor functions structure

---
+++ src/functions.py
@@ -0,0 +1,12 @@
+def sum2(a, b):
+	return a + b
+
+def minus2(a, b):
+	return a - b
+
+def multiply2(a, b):
+	return a * b
+
+def division2(a, b):
+	return a / b
+
--- src/main.py
+++ src/main.py
@@ -1,14 +1,2 @@
-def sum2(a, b):
-	return a + b
-
-def minus2(a, b):
-	return a - b
-
-def multiply2(a, b):
-	return a * b
-
-def division2(a, b):
-	return a / b
-	
 if __name__ == '__main__':
 	print('Hello')
